8fca733041e1544cdd30d6427eed68616ea64d70
migam830 <migam830@hotmail.com>
2023-11-29 22:57:39 +0000

    Added comments to RandomShuffler class

--- src/RandomShuffler.cpp
+++ src/RandomShuffler.cpp
@@ -3,11 +3,13 @@
 
 RandomShuffler::RandomShuffler() : randomDistribution(0, blockSelection.size() - 1)
 {
+	// Set seed to current system time
 	randomEngine.seed(std::chrono::system_clock::now().time_since_epoch().count());
 }
 
 char RandomShuffler::getNextPiece()
 {
+	// Generate random number of piece to spawn
 	int randomBlock = randomDistribution(randomEngine);
 	return blockSelection[randomBlock];
 }
--- src/RandomShuffler.h
+++ src/RandomShuffler.h
@@ -9,6 +9,7 @@ public:
 	char getNextPiece();
 
 private:
+	// Array to be selected from
 	const std::array<char, 7> blockSelection = { 'i', 'j', 'l', 'o', 's', 't', 'z' };
 	std::default_random_engine randomEngine;
 	std::uniform_int_distribution<> randomDistribution;
