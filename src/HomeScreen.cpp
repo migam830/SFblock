e3771bf285a67d351f2090506fcd3323c32c62b1
migam830 <migam830@hotmail.com>
2024-01-11 21:36:45 +0000

    Added simple button to start game

--- src/HomeScreen.cpp
+++ src/HomeScreen.cpp
@@ -7,6 +7,21 @@ HomeScreen::HomeScreen()
 	{
 		throw "Font not found";
 	}
+
+	// Set properties of text elements
+	gameButtonText.setFont(font);
+	gameButtonText.setPosition(290, 440);
+	gameButtonText.setOrigin(gameButtonText.getGlobalBounds().getSize() / 2.f + gameButtonText.getLocalBounds().getPosition());
+	gameButtonText.setCharacterSize(50);
+	gameButtonText.setFillColor(sf::Color::Black);
+	gameButtonText.setString("Start Game");
+
+	// Set properties of game button
+	gameButton.setPosition(225, 400);
+	gameButton.setSize(sf::Vector2f(350, 150));
+	gameButton.setFillColor(sf::Color(93, 173, 226));
+	gameButton.setOutlineColor(sf::Color::Black);
+	gameButton.setOutlineThickness(-10);
 }
 
 int HomeScreen::run(sf::RenderWindow& window)
@@ -27,12 +42,21 @@ int HomeScreen::run(sf::RenderWindow& window)
 				// Open the game screen
 				return 1;
 			}
+			if (event.type == sf::Event::MouseButtonPressed)
+			{
+				if (event.mouseButton.button == sf::Mouse::Left && gameButton.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
+				{
+					return 1;
+				}
+			}
 		}
 
 		// Clear window
 		window.clear(sf::Color::White);
 
 		// Draw everything here
+		window.draw(gameButton);
+		window.draw(gameButtonText);
 
 		// End current frame
 		window.display();
--- src/HomeScreen.h
+++ src/HomeScreen.h
@@ -8,6 +8,10 @@ public:
 	HomeScreen();
 
 private:
+	// All the UI elements of this screen
 	sf::Font font;
+	sf::RectangleShape gameButton;
+	sf::Text gameButtonText;
+
 	virtual int run(sf::RenderWindow& window);
 };
