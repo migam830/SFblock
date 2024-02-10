dca28ecf346ae1aca761cbd801f913b154a202a6
migam830 <migam830@hotmail.com>
2024-02-10 18:31:10 +0000

    Corrected comment in screen header file

--- src/Screen.h
+++ src/Screen.h
@@ -4,6 +4,6 @@
 class Screen
 {
 public:
-	// Function designed to be overridden so assigned to zero
+	// Function designed to be overridden so has 'equals zero' at the end
 	virtual int run(sf::RenderWindow& window) = 0;
 };
