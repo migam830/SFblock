e1f2ec6a08f4dd422d80bbc72d601875533926cf
migam830 <migam830@hotmail.com>
2024-02-10 17:07:36 +0000

    Changed screens vector to array

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,23 @@
 #include "GameScreen.h"
 #include "HomeScreen.h"
 #include <SFML/Graphics.hpp>
-#include <vector>
+#include <array>
 
 int main()
 {
-	// A dynamic array containing pointers to all the screens in the game
-	std::vector<Screen*> screens;
-	// The ID of the current screen
-	int currentScreen = 0;
-
+	// Initialise SFML window
 	sf::RenderWindow window(sf::VideoMode(800, 850), "SFblock");
 	window.setVerticalSyncEnabled(true);
 
 	// Instantiate screens here
 	HomeScreen screen1;
-	screens.push_back(&screen1);
 	GameScreen screen2;
-	screens.push_back(&screen2);
+
+	// An array containing pointers to all the screens in the game
+	std::array<Screen*, 2> screens = { &screen1, &screen2 };
+
+	// The ID of the current screen
+	int currentScreen = 0;
 
 	// Run current screen
 	while (currentScreen >= 0)
